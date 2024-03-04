0c79f29605a2188af6841404218a98cbaacb2d22
Mac <mac@MacdeMacBook-Pro.local>
2024-03-04 16:32:44 +0800

    第一次使用vue3,测试whistle

---
+++ src/views/test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>我是测试组件</div>
+  <div>{{a}}</div>
+  <div>{{b}}</div>
+
+  <div>用户名：<input v-model="userName" type="text"></div>
+  <div>密码：<input v-model="password" type="text"></div>
+  <button @click="login">login</button>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+var a:any = ref('')
+var b:any = ref('')
+ fetch('http://0.0.0.0:9000/getRequest',{method:'GET'}).then(res=>{
+     a.value = res||'get失败'
+ })
+ const userName = ref('')
+ const password = ref('')
+ const login=()=>{
+     fetch('http://0.0.0.0:9000/login',{method:'POST',body:JSON.stringify({
+        userName:userName.value,password:password.value
+     })}).then(res=>{
+        b.value = res||'post失败'
+     })
+ }
+fetch('http://jsonplaceholder.typicode.com/posts',{method:'GET'})
+
+
+
+</script>
+
+<style></style>
